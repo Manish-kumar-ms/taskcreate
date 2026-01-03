4c3e969537ade8e6fb779e54e57b6418863eaf34
Manish Kumar <ms5798034@gmail.com>
2026-01-03 13:29:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8080
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://taskcreate-frontend.onrender.com',
     credentials:true
 
 }))
